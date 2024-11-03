93e50f3177af9f3fbffd906ac373bfb4ed5c6a2e
Jonathan <joandste@abo.fi>
2024-11-03 03:28:41 +0200

    initial testing, chatgpt template though, will change

---
+++ src/alko/core.clj
@@ -0,0 +1,34 @@
+(ns alko.core
+  (:require [dk.ative.docjure.spreadsheet :as d]
+            [jsonista.core :as j]
+            [clojure.java.io :as io]))
+
+
+(def data (->> (d/load-workbook "alkon-hinnasto-tekstitiedostona.xlsx")
+               (d/select-sheet "Alkon Hinnasto Tekstitiedostona")
+               (d/select-columns {:A :id
+                                  :B :name
+                                  :D :size
+                                  :E :price
+                                  :F :price-per-liter
+                                  :I :type
+                                  :V :alkohol})
+               (drop 4)
+               (filter #(not= (get % :type) "lahja- ja juomatarvikkeet"))
+               (map (juxt :id :name :size :price :price-per-liter :type :alkohol))))
+
+(defn generate-html []
+  (let [json-data (j/write-value-as-string data)
+        template (slurp "template.html")
+        html-content (clojure.string/replace template "{{ data_placeholder }}" json-data)] 
+    (with-open [writer (io/writer "output.html")]
+      (.write writer html-content))))
+
+
+(defn -main []
+  (println "hello world"))
+
+
+(comment
+  (count data)
+  (generate-html))
